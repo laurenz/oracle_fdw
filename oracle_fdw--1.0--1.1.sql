9c32dc391699b8a4024bd0a20bb69be297696035
Laurenz Albe <laurenz.albe@wien.gv.at>
2014-04-14 14:40:13 +0200

    Add project web page.

--- oracle_fdw--1.0--1.1.sql
+++
@@ -1,6 +0,0 @@
-CREATE FUNCTION oracle_diag(name DEFAULT NULL) RETURNS text
-AS 'MODULE_PATHNAME'
-LANGUAGE C STABLE CALLED ON NULL INPUT;
-
-COMMENT ON FUNCTION oracle_diag(name)
-IS 'shows the version of oracle_fdw, Oracle client and Oracle server';
