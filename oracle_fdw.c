5445f8446f8eb6a821dca86d9c4ed00702ada934
vmora <vincent.mora@oslandia.com>
2014-09-17 17:48:56 +0200

    removed debug trace

--- oracle_fdw.c
+++ oracle_fdw.c
@@ -4390,11 +4390,7 @@ convertTuple(struct OracleFdwState *fdw_state, Datum *values, bool *nulls, bool 
 			}
 			else
 			{
-                            char msg[1000];
-
 				result = (bytea *)palloc(value_len + VARHDRSZ);
-                                sprintf(msg, "VARHDRSZ %d", VARHDRSZ);
-                                oracleDebug2(msg); 
 				oracleFillEWKB(fdw_state->session, geom, VARDATA(result));
                                 
 				SET_VARSIZE(result, value_len + VARHDRSZ);
