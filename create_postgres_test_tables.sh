deb6ca48632536aa8c91af582967d6d4e062d593
vmora <vincent.mora@oslandia.com>
2014-09-19 17:05:13 +0200

    added the "Z" in 3d wkt

--- create_postgres_test_tables.sh
+++ create_postgres_test_tables.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
+echo "DROP FOREIGN TABLE IF EXISTS gis CASCADE;"
+echo "CREATE FOREIGN TABLE gis (gid int OPTIONS (key 'true') NOT NULL, geom GEOMETRY) SERVER oradb OPTIONS (schema 'C##OSLANDIA', table 'GIS');"
+
 n=0
 while read line; do
     n=$(($n+1))
-    oraTab="geom_test_"$n
-    echo "DROP FOREIGN TABLE IF EXISTS geom_test_$n CASCADE;"
-    echo "CREATE FOREIGN TABLE geom_test_$n (geom GEOMETRY, gid int OPTIONS (key 'true') NOT NULL) SERVER oradb OPTIONS (schema 'C##OSLANDIA', table '${oraTab^^}');"
-    echo "SELECT 'result $n '||ST_AsText(geom) FROM geom_test_$n;"
-done < test_geometries.wkt
+
+    echo "INSERT INTO gis VALUES ($n,'SRID=8307;$line'::geometry);"
+done < test_geometrie.wkt
+
+echo "SELECT 'result',gid, ST_SRID(geom), ST_AsText(geom) FROM gis;"
