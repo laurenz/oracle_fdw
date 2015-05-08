laurenz/oracle_fdw

commit cb2b1d1cdca77cb3850ec285286e3fb3ecde7068

Improve cost estimates by using the row count estimate. Inspiration by Daniele Sevegnani.

oracle_fdw.c
@@ -783,11 +783,14 @@ oracleGetForeignRelSize(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntable
 	else
 	{
 		/* otherwise, use a random "high" value for cost */
-		fdwState->startup_cost = fdwState->total_cost = 10000.0;
+		fdwState->startup_cost = 10000.0;
 
 		/* if baserel->pages > 0, there was an ANALYZE; use the row count estimate */
 		if (baserel->pages > 0)
 			ntuples = baserel->tuples;
+
+		/* estimate total cost as startup cost + 10 * (row count) */
+		fdwState->total_cost = fdwState->startup_cost + ntuples * 10.0;
 
 		/* estimale selectivity locally for all conditions */
 		local_conditions = baserel->baserestrictinfo;

README.oracle_fdw
@@ -175,7 +175,7 @@ Foreign table options
   granularity).
   Since this cost estimate is almost useless but expensive to collect,
   oracle_fdw by default does not bother to get Oracle's cost estimates and
-  estimates the cost to 10000 regardless of the actual query.
+  estimates the cost to 10000+10*(row count) regardless of the actual query.
 
   From PostgreSQL 9.2 on, it is usually better to gather statistics on the
   foreign table (see ANALYZE below) and keep this option disabled.

commit ca9a22e76c1b418ace7acaa59dcc5e6907a5d078

Use the row count estimate for the total cost only if we have statistics. Otherwise the row count estimate is -1, which leads to a buggy total cost estimate. This bug was introduced in cb2b1d1cdca77cb3850ec285286e3fb3ecde7068.

oracle_fdw.c
@@ -789,8 +789,11 @@ oracleGetForeignRelSize(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntable
 		if (baserel->pages > 0)
 			ntuples = baserel->tuples;
 
-		/* estimate total cost as startup cost + 10 * (row count) */
-		fdwState->total_cost = fdwState->startup_cost + ntuples * 10.0;
+		/* if we have statistics, estimate total cost as startup cost + 10 * (row count) */
+		if (ntuples >= 0)
+			fdwState->total_cost = fdwState->startup_cost + ntuples * 10.0;
+		else
+			fdwState->total_cost = fdwState->startup_cost;
 
 		/* estimale selectivity locally for all conditions */
 		local_conditions = baserel->baserestrictinfo;