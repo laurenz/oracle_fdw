617bb11161f4eaafa002a3a5635a57995cfabb50
Laurenz Albe <laurenz.albe@wien.gv.at>
2016-04-25 15:55:24 +0200

    Adapt regression tests to fixed \det behaviour
    Since PostgreSQL commit 7e22470471e9ed7010fcbc4a18b0a461d088d7c7
    you have to use "\det schema.*" instead of "\det schema".

--- sql/oracle_import.sql
+++ sql/oracle_import.sql
@@ -4,6 +4,6 @@ CREATE SCHEMA import;
 
 IMPORT FOREIGN SCHEMA "SCOTT" LIMIT TO ("typetest1") FROM SERVER oracle INTO import OPTIONS (case 'lower');
 
-\det import
+\det import.*
 
 \d import.typetest1
