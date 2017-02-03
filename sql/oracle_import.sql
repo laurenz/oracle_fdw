851030b36ec94f48b6f0789db3b63c6bc0da3893
Laurenz Albe <laurenz.albe@wien.gv.at>
2017-02-03 16:04:13 +0100

    Make regression tests independent of psql commands
    The output format of \d has changed in PostgreSQL v10.
    Resort to directly querying the catalogs, which is arguably also
    not very stable, but it seems like an improvement from here.

--- sql/oracle_import.sql
+++ sql/oracle_import.sql
@@ -4,6 +4,15 @@ CREATE SCHEMA import;
 
 IMPORT FOREIGN SCHEMA "SCOTT" LIMIT TO ("typetest1") FROM SERVER oracle INTO import OPTIONS (case 'lower');
 
-\det import.*
+SELECT t.relname, fs.srvname, ft.ftoptions
+FROM pg_foreign_table ft
+     JOIN pg_class t ON ft.ftrelid = t.oid
+     JOIN pg_foreign_server fs ON ft.ftserver = fs.oid
+WHERE relnamespace = 'import'::regnamespace;
 
-\d import.typetest1
+SELECT attname, atttypid::regtype, attfdwoptions
+FROM pg_attribute
+WHERE attrelid = 'typetest1'::regclass
+  AND attnum > 0
+  AND NOT attisdropped
+ORDER BY attnum;
