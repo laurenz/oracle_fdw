d345134e07cebbf0689ffee067558bb16aa16b84
Laurenz Albe <laurenz.albe@wien.gv.at>
2017-05-31 11:32:24 +0200

    Report serialization failure if OCIStmtFetch2 does
    This case was already handled correctly in "oracleExecuteQuery",
    but not in "oracleFetchNext".
    Issue #170 shows that serialization failures can also occur while
    later rows are fetched, not only for the first row as I had assumed.

--- oracle_utils.c
+++ oracle_utils.c
@@ -2018,7 +2018,7 @@ oracleFetchNext(oracleSession *session)
 
 	if (result != OCI_SUCCESS && result != OCI_NO_DATA)
 	{
-		oracleError_d(FDW_UNABLE_TO_CREATE_EXECUTION,
+		oracleError_d(err_code == 8177 ? FDW_SERIALIZATION_FAILURE : FDW_UNABLE_TO_CREATE_EXECUTION,
 			"error fetching result: OCIStmtFetch2 failed to fetch next result row",
 			oraMessage);
 	}
