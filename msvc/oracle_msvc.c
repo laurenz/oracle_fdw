634410cd720848980f94fb8f8218d53e45918008
Christian Ullrich <chris@chrullrich.net>
2017-11-26 09:11:44 +0100

    Enable more useful diagnostics on Windows.
    By configuring oci.dll as delay-loaded, we can handle a failure loading it
    and produce a better error message.
    
    Without delay-loading, if oci.dll is not found, loading oracle_fdw itself
    fails, and the default error message in this situation mentions only
    oracle_fdw, causing the appearance that _it_ was not found rather than its
    dependency.

---
+++ msvc/oracle_msvc.c
@@ -0,0 +1,59 @@
+#include "..\oracle_fdw.h"
+
+#include "postgres.h"
+#include "utils\elog.h"
+
+#include <windows.h>
+
+#pragma warning(push)
+#pragma warning(disable: 4201)
+#include <delayimp.h>
+#pragma warning(pop)
+
+#define ERRMSG_LIB "Oracle client library (oci.dll) not found"
+#define ERRDETAIL_LIB "No Oracle client is installed, or your system is configured incorrectly."
+#define ERRMSG_PROC "Incompatible version of Oracle client library (oci.dll) found"
+#define ERRDETAIL_PROC "An exported function was not found in oci.dll."
+
+FARPROC WINAPI
+oracleDelayLoadFailureHook(unsigned dliNotify, PDelayLoadInfo pdli)
+{
+	if (dliNotify == dliFailLoadLib) {
+#if defined(ORAFDW_INSECURE_DIAG)
+		ereport(ERROR,
+			(errcode(ERRCODE_SYSTEM_ERROR),
+			errmsg(ERRMSG_LIB),
+			errdetail(ERRDETAIL_LIB),
+			errhint("The current PATH is: %s", getenv("PATH"))));
+#else
+		ereport(ERROR,
+			(errcode(ERRCODE_SYSTEM_ERROR),
+			errmsg(ERRMSG_LIB),
+			errdetail(ERRDETAIL_LIB),
+			errhint("Verify that the PATH variable includes the Oracle client.")));
+#endif	/* ORAFDW_INSECURE_DIAG */
+	}
+	else if (dliNotify == dliFailGetProc) {
+		/* There are no exports by ordinal yets. */
+		if (pdli->dlp.fImportByName) {
+			ereport(ERROR,
+				(errcode(ERRCODE_SYSTEM_ERROR),
+				errmsg(ERRMSG_PROC),
+				errdetail(ERRDETAIL_PROC),
+				errhint("Missing function: %s", pdli->dlp.szProcName)));
+		}
+		else {
+			ereport(ERROR,
+				(errcode(ERRCODE_SYSTEM_ERROR),
+				errmsg(ERRMSG_PROC),
+				errdetail(ERRDETAIL_PROC),
+				errhint("Missing ordinal: #%u", pdli->dlp.dwOrdinal)));
+		}
+	}
+	return 0;
+}
+
+#if _MSC_VER >= 1900
+const
+#endif
+PfnDliHook __pfnDliFailureHook2 = oracleDelayLoadFailureHook;
