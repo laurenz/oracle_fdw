laurenz/oracle_fdw

commit 77242d1fef780e3f1b77561c81542889a83fb334

Fix NCHAR and NVARCHAR2 handling
By default, Oracle converts NCHAR and NVARCHAR2 columns
(which are stored in a UNICODE encoding) to the database
encoding before converting them to the client encoding.

If the database encoding is not AL32UTF8 (which is the
main use case for the now obsolete NCHAR and NVARCHAR2
data types), converting to the database encoding will
substitute characters that cannot be represented with
"replacement characters", which results in data loss.

Reported by "srakazmus", who also found that you need
to use OCI_NCHAR_LITERAL_REPLACE_ON in OCIEnvCreate.

Special thanks to Christian Ullrich, who suspected that
OCI_ATTR_CHARSET_FORM needs to be set to SQLCS_NCHAR in
the bind and define handles to make this work.

Unfortunately the Oracle documentation is unclear and
contradictory on this point, so we had to use trial and
error.

This closes #416.

oracle_utils.c
@@ -162,7 +162,8 @@ oracleSession
 
 		/* create environment handle */
 		if (checkerr(
-			OCIEnvCreate((OCIEnv **) &envhp, (ub4)OCI_OBJECT,
+			OCIEnvCreate((OCIEnv **) &envhp,
+				(ub4)OCI_OBJECT | OCI_NCHAR_LITERAL_REPLACE_ON,
 				(dvoid *) 0, (dvoid * (*)(dvoid *,size_t)) 0,
 				(dvoid * (*)(dvoid *, dvoid *, size_t)) 0,
 				(void (*)(dvoid *, dvoid *)) 0, (size_t) 0, (dvoid **) 0),
@@ -1666,6 +1667,7 @@ oraclePrepareQuery(oracleSession *session, const char *query, const struct oraTa
 	static sb4 dummy_size = 4;
 	static sb2 dummy_null;
 	ub4 prefetch_rows = prefetch;
+	const ub1 nchar = SQLCS_NCHAR;
 
 	/* figure out if the query is FOR UPDATE */
 	is_select = (strncmp(query, "SELECT", 6) == 0);
@@ -1736,6 +1738,16 @@ oraclePrepareQuery(oracleSession *session, const char *query, const struct oraTa
 				{
 					oracleError_d(FDW_UNABLE_TO_CREATE_EXECUTION,
 						"error executing query: OCIDefineByPos failed to define result value",
+						oraMessage);
+				}
+
+				if (checkerr(
+					OCIAttrSet((void *)defnhp, OCI_HTYPE_DEFINE, (void *)&nchar, 0,
+						OCI_ATTR_CHARSET_FORM, session->envp->errhp),
+					(dvoid *)session->envp->errhp, OCI_HTYPE_ERROR) != OCI_SUCCESS)
+				{
+					oracleError_d(FDW_UNABLE_TO_CREATE_EXECUTION,
+						"error executing query: OCIAttrSet failed to set charset form on result value",
 						oraMessage);
 				}
 
@@ -1831,6 +1843,7 @@ oracleExecuteQuery(oracleSession *session, const struct oraTable *oraTable, stru
 	sword result;
 	ub4 rowcount;
 	int param_count = 0;
+	const ub1 nchar = SQLCS_NCHAR;
 
 	for (param=paramList; param; param=param->next)
 		++param_count;
@@ -1957,6 +1970,16 @@ oracleExecuteQuery(oracleSession *session, const struct oraTable *oraTable, stru
 		{
 			oracleError_d(FDW_UNABLE_TO_CREATE_EXECUTION,
 				"error executing query: OCIBindByName failed to bind parameter",
+				oraMessage);
+		}
+
+		if (checkerr(
+			OCIAttrSet((void *)param->bindh, OCI_HTYPE_BIND, (void *)&nchar, 0,
+				OCI_ATTR_CHARSET_FORM, session->envp->errhp),
+			(dvoid *)session->envp->errhp, OCI_HTYPE_ERROR) != OCI_SUCCESS)
+		{
+			oracleError_d(FDW_UNABLE_TO_CREATE_EXECUTION,
+				"error executing query: OCIAttrSet failed to set charset form on bind parameter",
 				oraMessage);
 		}
 

commit 5a3ce2da85cc722e44ca25028467fe6cd0133749

Fix performance regression introduced in 77242d1fef
Setting OCI_ATTR_CHARSET_FORM to SQLCS_NCHAR makes Oracle handle
encoding conversion for columns with a "national character set"
correctly, but it causes a severe performance impact, as reported
by Phil Florent in #434.

So improve the code to set the property only on define handles
for such Oracle columns.

The case of bind handles is left as it is - our DML performance
is not competitive anyway, and it would require tracking if a
parameter is bound to an Oracle column where it is needed.

This closes #434.

oracle_fdw.h
@@ -15,7 +15,7 @@
 #include <sys/types.h>
 
 /* oracle_fdw version */
-#define ORACLE_FDW_VERSION "2.3.0"
+#define ORACLE_FDW_VERSION "2.3.1devel"
 
 #ifdef OCI_ORACLE
 /*

oracle_utils.c
@@ -1712,6 +1712,7 @@ oraclePrepareQuery(oracleSession *session, const char *query, const struct oraTa
 			if (is_select)
 			{
 				ub2 type;
+				oraType oracle_type = oraTable->cols[i]->oratype;
 
 				/* figure out in which format we want the results */
 				type = getOraType(oraTable->cols[i]->oratype);
@@ -1741,10 +1742,15 @@ oraclePrepareQuery(oracleSession *session, const char *query, const struct oraTa
 						oraMessage);
 				}
 
-				if (checkerr(
-					OCIAttrSet((void *)defnhp, OCI_HTYPE_DEFINE, (void *)&nchar, 0,
-						OCI_ATTR_CHARSET_FORM, session->envp->errhp),
-					(dvoid *)session->envp->errhp, OCI_HTYPE_ERROR) != OCI_SUCCESS)
+				/*
+				 * Use the more expensive character set conversion only for columns
+				 * using a national character set, where it is needed.
+				 */
+				if ((oracle_type == ORA_TYPE_NVARCHAR2 || oracle_type == ORA_TYPE_NCHAR)
+					&& checkerr(
+						OCIAttrSet((void *)defnhp, OCI_HTYPE_DEFINE, (void *)&nchar, 0,
+							OCI_ATTR_CHARSET_FORM, session->envp->errhp),
+						(dvoid *)session->envp->errhp, OCI_HTYPE_ERROR) != OCI_SUCCESS)
 				{
 					oracleError_d(FDW_UNABLE_TO_CREATE_EXECUTION,
 						"error executing query: OCIAttrSet failed to set charset form on result value",
@@ -1973,6 +1979,14 @@ oracleExecuteQuery(oracleSession *session, const struct oraTable *oraTable, stru
 				oraMessage);
 		}
 
+		/*
+		 * Always use the more expensive character conversion, although this is
+		 * only required when we are dealing with "national character sets" on
+		 * the Oracle side.
+		 * There is room for improvement here, but that would require tracking
+		 * if the parameter corresponds to such a column in an Oracle table.
+		 * And DML performance is already something where we are not great...
+		 */
 		if (checkerr(
 			OCIAttrSet((void *)param->bindh, OCI_HTYPE_BIND, (void *)&nchar, 0,
 				OCI_ATTR_CHARSET_FORM, session->envp->errhp),