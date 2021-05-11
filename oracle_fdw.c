laurenz/oracle_fdw

commit 7720b7db878e85991085f861484ddb0534e6069d

Replace call to deparseExpr to extract value of the LIMIT and OFFSET clause by looking directly at the constant value.
Improve check on valid limitOfsset value.

expected/oracle_fdw.out
@@ -5,7 +5,7 @@
 SET client_min_messages = WARNING;
 CREATE EXTENSION oracle_fdw;
 -- TWO_TASK or ORACLE_HOME and ORACLE_SID must be set in the server's environment for this to work
-CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '//192.168.1.34:1521/orcl', isolation_level 'read_committed', nchar 'true');
+CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '', isolation_level 'read_committed', nchar 'true');
 CREATE USER MAPPING FOR PUBLIC SERVER oracle OPTIONS (user 'SCOTT', password 'tiger');
 -- drop the Oracle tables if they exist
 DO

oracle_fdw.c
@@ -754,20 +754,10 @@ deparseLimit(PlannerInfo *root, struct OracleFdwState *fdwState, RelOptInfo *bas
 			char *offset_val;
 
 			appendStringInfoString(&limit_clause, " FETCH FIRST ");
-			limit_val = deparseExpr(
-						fdwState->session, baserel,
-						(Expr *) root->parse->limitCount,
-						fdwState->oraTable,
-						&(fdwState->params)
-					);
-			if (root->parse->limitOffset != NULL)
+			limit_val = datumToString(((Const *) root->parse->limitCount)->constvalue, ((Const *) root->parse->limitCount)->consttype);
+			if (IsA(root->parse->limitOffset, Const) && !((Const *) root->parse->limitOffset)->constisnull)
 			{
-				offset_val = deparseExpr(
-							fdwState->session, baserel,
-							(Expr *) root->parse->limitOffset,
-							fdwState->oraTable,
-							&(fdwState->params)
-						);
+				offset_val = datumToString(((Const *) root->parse->limitOffset)->constvalue, ((Const *) root->parse->limitOffset)->consttype);
 				appendStringInfo(&limit_clause, "%s+%s", limit_val, offset_val);
 			}
 			else

commit ee0bf969b54e20f697df2ab93d1c8257583b43af

Fix unwanted pushdown of LILIT clause when an aggregate function is used as they are not shipped to Oracle server.
Fix crash of oracle_fdw when there was not OFFSET clause introduced with
commit 7720b7d.

expected/oracle_fdw.out
@@ -5,7 +5,7 @@
 SET client_min_messages = WARNING;
 CREATE EXTENSION oracle_fdw;
 -- TWO_TASK or ORACLE_HOME and ORACLE_SID must be set in the server's environment for this to work
-CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '', isolation_level 'read_committed', nchar 'true');
+CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '//192.168.1.34:1521/orcl', isolation_level 'read_committed', nchar 'true');
 CREATE USER MAPPING FOR PUBLIC SERVER oracle OPTIONS (user 'SCOTT', password 'tiger');
 -- drop the Oracle tables if they exist
 DO
@@ -731,4 +731,19 @@ SELECT d, count(*) FROM typetest1 GROUP BY d LIMIT 2;
  03-15-0044 BC |     1
  10-21-1968    |     1
 (2 rows)
+
+EXPLAIN (COSTS OFF) SELECT count(*) FROM typetest1 LIMIT 1;
+                                           QUERY PLAN                                            
+-------------------------------------------------------------------------------------------------
+ Limit
+   ->  Aggregate
+         ->  Foreign Scan on typetest1
+               Oracle query: SELECT /*50857ae5af0c5a170837c8c56f91e583*/ '1' FROM "TYPETEST1" r1
+(4 rows)
+
+SELECT count(*) FROM typetest1 LIMIT 1;
+ count 
+-------
+     3
+(1 row)
 

oracle_fdw.c
@@ -752,19 +752,37 @@ deparseLimit(PlannerInfo *root, struct OracleFdwState *fdwState, RelOptInfo *bas
 		{
 			char *limit_val;
 			char *offset_val;
+			int add_limit_clause = true;
+			ListCell   *lc;
 
-			appendStringInfoString(&limit_clause, " FETCH FIRST ");
-			limit_val = datumToString(((Const *) root->parse->limitCount)->constvalue, ((Const *) root->parse->limitCount)->consttype);
-			if (IsA(root->parse->limitOffset, Const) && !((Const *) root->parse->limitOffset)->constisnull)
+			/* we don't append the LIMIT clause if there is aggregate function used */
+			foreach (lc, root->processed_tlist)
 			{
-				offset_val = datumToString(((Const *) root->parse->limitOffset)->constvalue, ((Const *) root->parse->limitOffset)->consttype);
-				appendStringInfo(&limit_clause, "%s+%s", limit_val, offset_val);
+				Node *ptl = (Node *) lfirst(lc);
+				if (IsA(ptl, TargetEntry))
+				{
+					TargetEntry *tge = (TargetEntry *) ptl;
+					if (IsA(tge->expr, Aggref))
+						add_limit_clause = false;
+				}
 			}
-			else
-				appendStringInfo(&limit_clause, "%s", limit_val);
-			appendStringInfoString(&limit_clause, " ROWS ONLY");
 
-			return limit_clause.data;
+			if (add_limit_clause)
+			{
+				appendStringInfoString(&limit_clause, " FETCH FIRST ");
+				limit_val = datumToString(((Const *) root->parse->limitCount)->constvalue, ((Const *) root->parse->limitCount)->consttype);
+				if (root->parse->limitOffset != NULL && IsA(root->parse->limitOffset, Const)
+						&& !((Const *) root->parse->limitOffset)->constisnull)
+				{
+					offset_val = datumToString(((Const *) root->parse->limitOffset)->constvalue, ((Const *) root->parse->limitOffset)->consttype);
+					appendStringInfo(&limit_clause, "%s+%s", limit_val, offset_val);
+				}
+				else
+					appendStringInfo(&limit_clause, "%s", limit_val);
+				appendStringInfoString(&limit_clause, " ROWS ONLY");
+
+				return limit_clause.data;
+			}
 		}
 	}
 

sql/oracle_fdw.sql
@@ -8,7 +8,7 @@ SET client_min_messages = WARNING;
 CREATE EXTENSION oracle_fdw;
 
 -- TWO_TASK or ORACLE_HOME and ORACLE_SID must be set in the server's environment for this to work
-CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '', isolation_level 'read_committed', nchar 'true');
+CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '//192.168.1.34:1521/orcl', isolation_level 'read_committed', nchar 'true');
 
 CREATE USER MAPPING FOR PUBLIC SERVER oracle OPTIONS (user 'SCOTT', password 'tiger');
 
@@ -465,3 +465,5 @@ SELECT * FROM qtest LIMIT 1 OFFSET 2;
 -- When a GROUP BY clause is present the LIMIT clause is not pushed
 EXPLAIN (VERBOSE on, COSTS off) SELECT d, count(*) FROM typetest1 GROUP BY d LIMIT 2;
 SELECT d, count(*) FROM typetest1 GROUP BY d LIMIT 2;
+EXPLAIN (COSTS OFF) SELECT count(*) FROM typetest1 LIMIT 1;
+SELECT count(*) FROM typetest1 LIMIT 1;