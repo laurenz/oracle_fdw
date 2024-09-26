laurenz/oracle_fdw

commit d62b87614935fdef22ae79661b9b39b7491471dd

Fix crash or empty result when reading larger CLOBs
Found by trial and error; the Oracle documentation
for OCILobRead2() is less than clear.

Report by Rainer Hartwig and Erik van der Vlist.
This fixes #614 and fixes #643.

oracle_utils.c
@@ -2392,8 +2392,8 @@ oracleGetLob(oracleSession *session, void *locptr, oraType type, char **value, l
 		 * On subsequent reads, the amount_* parameters are ignored.
 		 * After the call, "amount_byte" contains the number of bytes read.
 		 */
-		amount_byte = lobsize;      /* ignored for CLOBs */
-		amount_char = amount_byte;  /* ignored for binary LOBs */
+		amount_byte = 0;
+		amount_char = 0;
 		result = checkerr(
 			OCILobRead2(session->connp->svchp, session->envp->errhp, locp, &amount_byte, &amount_char,
 				(oraub8)1, (dvoid *)(*value + *value_len), lob_buf_size,

commit a95c3434ed3b4072706d78142e4ac83a2fd62704

Don't fetch more of a LOB than is needed
The change in commit d62b876149 inadvertently caused the whole
LOB to be fetched, even if we only need part of it (for ANALYZE).

Improve matters by exiting the loop as soon as we have enough.
This may still fetch the whole LOB from Oracle, but we can at
least only copy as much as we need.

Noticed by "shaunxzy" in #707.

oracle_utils.c
@@ -2331,6 +2331,7 @@ oracleGetLob(oracleSession *session, void *locptr, oraType type, char **value, l
 	OCILobLocator *locp = *(OCILobLocator **)locptr;
 	oraub8 amount_byte, amount_char, lobsize;
 	sword result = OCI_SUCCESS;
+	unsigned long chars_received = 0;
 
 	/* initialize result buffer length */
 	*value_len = 0;
@@ -2410,6 +2411,13 @@ oracleGetLob(oracleSession *session, void *locptr, oraType type, char **value, l
 
 		/* update LOB length */
 		*value_len += (long)amount_byte;
+		chars_received += amount_char;
+
+		/* stop reading data if we got at least "trunc" bytes or characters */
+		if (trunc != 0 &&
+			((type == ORA_TYPE_CLOB && chars_received >= trunc) ||
+			 *value_len >= trunc))
+			break;
 	}
 	while (result == OCI_NEED_DATA);
 