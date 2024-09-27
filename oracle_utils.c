laurenz/oracle_fdw

commit 5fd556f60274c6c287104f0a2aa90b8075e663cb

Introduce a server option "nchar", turned "off" by default
This option, if set, enables the fix for NCHAR and NVARCHAR2 columns
introduced in 77242d1fef.

We don't want that by default, since it causes a noticable performance
hit (see #435 and #451) and makes UPDATEs with long strings fail
with ORA-01461 (#453).

Problem reports by "threenotrump", "CHunter" and Phil Florent.

expected/oracle_fdw.out
@@ -5,7 +5,7 @@
 SET client_min_messages = WARNING;
 CREATE EXTENSION oracle_fdw;
 -- TWO_TASK or ORACLE_HOME and ORACLE_SID must be set in the server's environment for this to work
-CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '', isolation_level 'read_committed');
+CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '', isolation_level 'read_committed', nchar 'true');
 CREATE USER MAPPING FOR PUBLIC SERVER oracle OPTIONS (user 'SCOTT', password 'tiger');
 -- drop the Oracle tables if they exist
 DO

oracle_utils.c
@@ -86,7 +86,9 @@ static void setNullGeometry(oracleSession *session, ora_geometry *geom);
  * 		"curlevel" is the current PostgreSQL transaction level.
  */
 oracleSession
-*oracleGetSession(const char *connectstring, oraIsoLevel isolation_level, char *user, char *password, const char *nls_lang, const char *tablename, int curlevel)
+*oracleGetSession(
+	const char *connectstring, oraIsoLevel isolation_level, char *user, char *password,
+	const char *nls_lang, int have_nchar, const char *tablename, int curlevel)
 {
 	OCIEnv *envhp = NULL;
 	OCIError *errhp = NULL;
@@ -147,6 +149,7 @@ oracleSession
 
 	if (envhp == NULL)
 	{
+		ub4 handle_mode;
 		/*
 		 * Create environment and error handle.
 		 */
@@ -160,10 +163,15 @@ oracleSession
 		/* set Oracle environment */
 		setOracleEnvironment(nlscopy);
 
+		/* use OCI_NCHAR_LITERAL_REPLACE_ON only if we have to convert national characters */
+		if (have_nchar)
+			handle_mode = OCI_OBJECT | OCI_NCHAR_LITERAL_REPLACE_ON;
+		else
+			handle_mode = OCI_OBJECT;
+
 		/* create environment handle */
 		if (checkerr(
-			OCIEnvCreate((OCIEnv **) &envhp,
-				(ub4)OCI_OBJECT | OCI_NCHAR_LITERAL_REPLACE_ON,
+			OCIEnvCreate((OCIEnv **) &envhp, handle_mode,
 				(dvoid *) 0, (dvoid * (*)(dvoid *,size_t)) 0,
 				(dvoid * (*)(dvoid *, dvoid *, size_t)) 0,
 				(void (*)(dvoid *, dvoid *)) 0, (size_t) 0, (dvoid **) 0),
@@ -540,6 +548,7 @@ oracleSession
 	session->srvp = srvp;
 	session->connp = connp;
 	session->stmthp = NULL;
+	session->have_nchar = have_nchar;
 
 	/* set savepoints up to the current level */
 	oracleSetSavepoint(session, curlevel);
@@ -1743,10 +1752,11 @@ oraclePrepareQuery(oracleSession *session, const char *query, const struct oraTa
 				}
 
 				/*
-				 * Use the more expensive character set conversion only for columns
-				 * using a national character set, where it is needed.
+				 * Use the more expensive character set conversion only if requested and only
+				 * for columns with a national character set.
 				 */
-				if ((oracle_type == ORA_TYPE_NVARCHAR2 || oracle_type == ORA_TYPE_NCHAR)
+				if (session->have_nchar
+					&& (oracle_type == ORA_TYPE_NVARCHAR2 || oracle_type == ORA_TYPE_NCHAR)
 					&& checkerr(
 						OCIAttrSet((void *)defnhp, OCI_HTYPE_DEFINE, (void *)&nchar, 0,
 							OCI_ATTR_CHARSET_FORM, session->envp->errhp),
@@ -1980,17 +1990,14 @@ oracleExecuteQuery(oracleSession *session, const struct oraTable *oraTable, stru
 		}
 
 		/*
-		 * Always use the more expensive character conversion, although this is
-		 * only required when we are dealing with "national character sets" on
-		 * the Oracle side.
-		 * There is room for improvement here, but that would require tracking
-		 * if the parameter corresponds to such a column in an Oracle table.
-		 * And DML performance is already something where we are not great...
+		 * Use the expensive character conversion only if we are dealing with
+		 * "national character sets" on the Oracle side.
 		 */
-		if (checkerr(
-			OCIAttrSet((void *)param->bindh, OCI_HTYPE_BIND, (void *)&nchar, 0,
-				OCI_ATTR_CHARSET_FORM, session->envp->errhp),
-			(dvoid *)session->envp->errhp, OCI_HTYPE_ERROR) != OCI_SUCCESS)
+		if (session->have_nchar
+			&& checkerr(
+				OCIAttrSet((void *)param->bindh, OCI_HTYPE_BIND, (void *)&nchar, 0,
+					OCI_ATTR_CHARSET_FORM, session->envp->errhp),
+				(dvoid *)session->envp->errhp, OCI_HTYPE_ERROR) != OCI_SUCCESS)
 		{
 			oracleError_d(FDW_UNABLE_TO_CREATE_EXECUTION,
 				"error executing query: OCIAttrSet failed to set charset form on bind parameter",

README.oracle_fdw
@@ -183,6 +183,18 @@ Foreign server options
   is a risk of inconsistencies**.  If you want to use it, check your
   execution plans if the foreign scan could be executed more than once.
 
+- **nchar** (boolean, optional, defaults to `off`)
+
+  Setting this option to `on` chooses a more expensive character conversion
+  on the Oracle side.  This is required if you are using a single-byte
+  Oracle database character set, but have NCHAR or NVARCHAR2 columns
+  containing contain characters that cannot be represented in the database
+  character set.
+
+  This has a noticable performance impact, and it causes ORA-01461 errors
+  with UPDATE statements that set strings over 2000 bytes (or 16383 if you
+  have MAX_STRING_SIZE = EXTENDED).  This error seems to be an Oracle bug.
+
 User mapping options
 --------------------
 

oracle_fdw.c
@@ -159,6 +159,7 @@ struct OracleFdwOption
 #define OPT_NLS_LANG "nls_lang"
 #define OPT_DBSERVER "dbserver"
 #define OPT_ISOLATION_LEVEL "isolation_level"
+#define OPT_NCHAR "nchar"
 #define OPT_USER "user"
 #define OPT_PASSWORD "password"
 #define OPT_DBLINK "dblink"
@@ -187,6 +188,7 @@ static struct OracleFdwOption valid_options[] = {
 	{OPT_NLS_LANG, ForeignDataWrapperRelationId, false},
 	{OPT_DBSERVER, ForeignServerRelationId, true},
 	{OPT_ISOLATION_LEVEL, ForeignServerRelationId, false},
+	{OPT_NCHAR, ForeignServerRelationId, false},
 	{OPT_USER, UserMappingRelationId, true},
 	{OPT_PASSWORD, UserMappingRelationId, true},
 	{OPT_DBLINK, ForeignTableRelationId, false},
@@ -224,6 +226,7 @@ struct OracleFdwState {
 	char *user;                    /* Oracle username */
 	char *password;                /* Oracle password */
 	char *nls_lang;                /* Oracle locale information */
+	bool have_nchar;               /* needs support for national character conversion */
 	oracleSession *session;        /* encapsulates the active Oracle session */
 	char *query;                   /* query we issue against Oracle */
 	List *params;                  /* list of parameters needed for the query */
@@ -478,10 +481,11 @@ oracle_fdw_validator(PG_FUNCTION_ARGS)
 		if (strcmp(def->defname, OPT_ISOLATION_LEVEL) == 0)
 			(void)getIsolationLevel(((Value *)(def->arg))->val.str);
 
-		/* check valid values for "readonly" and "key" */
+		/* check valid values for "readonly", "key", "strip_zeros" and "nchar" */
 		if (strcmp(def->defname, OPT_READONLY) == 0
 				|| strcmp(def->defname, OPT_KEY) == 0
 				|| strcmp(def->defname, OPT_STRIP_ZEROS) == 0
+				|| strcmp(def->defname, OPT_NCHAR) == 0
 			)
 		{
 			char *val = ((Value *)(def->arg))->val.str;
@@ -1298,6 +1302,7 @@ oracleBeginForeignScan(ForeignScanState *node, int eflags)
 			fdw_state->user,
 			fdw_state->password,
 			fdw_state->nls_lang,
+			(int)fdw_state->have_nchar,
 			fdw_state->oraTable->pgname,
 			GetCurrentTransactionNestLevel()
 		);
@@ -1729,6 +1734,7 @@ oracleBeginForeignModify(ModifyTableState *mtstate, ResultRelInfo *rinfo, List *
 			fdw_state->user,
 			fdw_state->password,
 			fdw_state->nls_lang,
+			(int)fdw_state->have_nchar,
 			fdw_state->oraTable->pgname,
 			GetCurrentTransactionNestLevel()
 		);
@@ -1809,6 +1815,7 @@ void oracleBeginForeignInsert(ModifyTableState *mtstate, ResultRelInfo *rinfo)
 			fdw_state->user,
 			fdw_state->password,
 			fdw_state->nls_lang,
+			(int)fdw_state->have_nchar,
 			fdw_state->oraTable->pgname,
 			GetCurrentTransactionNestLevel()
 		);
@@ -2117,6 +2124,7 @@ oracleImportForeignSchema(ImportForeignSchemaStmt *stmt, Oid serverOid)
 	bool readonly = false, firstcol = true;
 	int collation = DEFAULT_COLLATION_OID;
 	oraIsoLevel isolation_level_val = DEFAULT_ISOLATION_LEVEL;
+	bool have_nchar = false;
 
 	/* get the foreign server, the user mapping and the FDW */
 	server = GetForeignServer(serverOid);
@@ -2141,6 +2149,15 @@ oracleImportForeignSchema(ImportForeignSchemaStmt *stmt, Oid serverOid)
 			user = ((Value *) (def->arg))->val.str;
 		if (strcmp(def->defname, OPT_PASSWORD) == 0)
 			password = ((Value *) (def->arg))->val.str;
+		if (strcmp(def->defname, OPT_NCHAR) == 0)
+		{
+			char *nchar = ((Value *) (def->arg))->val.str;
+
+			if (pg_strcasecmp(nchar, "on") == 0
+					|| pg_strcasecmp(nchar, "yes") == 0
+					|| pg_strcasecmp(nchar, "true") == 0)
+				have_nchar = true;
+		}
 	}
 
 	/* process the options of the IMPORT FOREIGN SCHEMA command */
@@ -2283,6 +2300,7 @@ oracleImportForeignSchema(ImportForeignSchemaStmt *stmt, Oid serverOid)
 		user,
 		password,
 		nls_lang,
+		(int)have_nchar,
 		NULL,
 		1
 	);
@@ -2463,7 +2481,8 @@ struct OracleFdwState
 	List *options;
 	ListCell *cell;
 	char *isolationlevel = NULL;
-	char *dblink = NULL, *schema = NULL, *table = NULL, *maxlong = NULL, *sample = NULL, *fetch = NULL;
+	char *dblink = NULL, *schema = NULL, *table = NULL, *maxlong = NULL,
+		 *sample = NULL, *fetch = NULL, *nchar = NULL;
 	long max_long;
 
 	/*
@@ -2496,6 +2515,8 @@ struct OracleFdwState
 			sample = ((Value *) (def->arg))->val.str;
 		if (strcmp(def->defname, OPT_PREFETCH) == 0)
 			fetch = ((Value *) (def->arg))->val.str;
+		if (strcmp(def->defname, OPT_NCHAR) == 0)
+			nchar = ((Value *) (def->arg))->val.str;
 	}
 
     /* set isolation_level (or use default) */
@@ -2525,6 +2546,15 @@ struct OracleFdwState
 	else
 		fdwState->prefetch = (unsigned int)strtoul(fetch, NULL, 0);
 
+	/* convert "nchar" option to boolean (or use "false") */
+	if (nchar != NULL
+		&& (pg_strcasecmp(nchar, "on") == 0
+			|| pg_strcasecmp(nchar, "yes") == 0
+			|| pg_strcasecmp(nchar, "true") == 0))
+		fdwState->have_nchar = true;
+	else
+		fdwState->have_nchar = false;
+
 	/* check if options are ok */
 	if (table == NULL)
 		ereport(ERROR,
@@ -2541,6 +2571,7 @@ struct OracleFdwState
 		fdwState->user,
 		fdwState->password,
 		fdwState->nls_lang,
+		(int)fdwState->have_nchar,
 		pgtablename,
 		GetCurrentTransactionNestLevel()
 	);
@@ -3131,6 +3162,7 @@ foreign_join_ok(PlannerInfo *root, RelOptInfo *joinrel, JoinType jointype,
 	fdwState->user     = fdwState_o->user;
 	fdwState->password = fdwState_o->password;
 	fdwState->nls_lang = fdwState_o->nls_lang;
+	fdwState->have_nchar = fdwState_o->have_nchar;
 
 	foreach(lc, pull_var_clause((Node *)joinrel->reltarget->exprs, PVC_RECURSE_PLACEHOLDERS))
 	{
@@ -5071,6 +5103,7 @@ oracleConnectServer(Name srvname)
 	ListCell *cell;
 	char *nls_lang = NULL, *user = NULL, *password = NULL, *dbserver = NULL;
 	oraIsoLevel isolation_level = DEFAULT_ISOLATION_LEVEL;
+	bool have_nchar = false;
 
 	/* look up foreign server with this name */
 	rel = table_open(ForeignServerRelationId, AccessShareLock);
@@ -5107,11 +5140,20 @@ oracleConnectServer(Name srvname)
 		if (strcmp(def->defname, OPT_DBSERVER) == 0)
 			dbserver = ((Value *) (def->arg))->val.str;
 		if (strcmp(def->defname, OPT_ISOLATION_LEVEL) == 0)
-		isolation_level = getIsolationLevel(((Value *) (def->arg))->val.str);
+			isolation_level = getIsolationLevel(((Value *) (def->arg))->val.str);
 		if (strcmp(def->defname, OPT_USER) == 0)
 			user = ((Value *) (def->arg))->val.str;
 		if (strcmp(def->defname, OPT_PASSWORD) == 0)
 			password = ((Value *) (def->arg))->val.str;
+		if (strcmp(def->defname, OPT_NCHAR) == 0)
+		{
+			char *nchar = ((Value *) (def->arg))->val.str;
+
+			if ((pg_strcasecmp(nchar, "on") == 0
+				|| pg_strcasecmp(nchar, "yes") == 0
+				|| pg_strcasecmp(nchar, "true") == 0))
+			have_nchar = true;
+		}
 	}
 
 	/* guess a good NLS_LANG environment setting */
@@ -5124,6 +5166,7 @@ oracleConnectServer(Name srvname)
 		user,
 		password,
 		nls_lang,
+		(int)have_nchar,
 		NULL,
 		1
 	);
@@ -5150,6 +5193,8 @@ List
 	result = lappend(result, serializeString(fdwState->dbserver));
 	/* isolation_level */
 	result = lappend(result, serializeInt((int)fdwState->isolation_level));
+	/* have_nchar */
+	result = lappend(result, serializeInt((int)fdwState->have_nchar));
 	/* user name */
 	result = lappend(result, serializeString(fdwState->user));
 	/* password */
@@ -5270,6 +5315,10 @@ struct OracleFdwState
 
 	/* isolation_level */
 	state->isolation_level = (oraIsoLevel)DatumGetInt32(((Const *)lfirst(cell))->constvalue);
+	cell = list_next(list, cell);
+
+	/* have_nchar */
+	state->have_nchar = (bool)DatumGetInt32(((Const *)lfirst(cell))->constvalue);
 	cell = list_next(list, cell);
 
 	/* user */
@@ -5596,6 +5645,7 @@ struct OracleFdwState
 
 	copy->dbserver = pstrdup(orig->dbserver);
 	copy->isolation_level = orig->isolation_level;
+	copy->have_nchar = orig->have_nchar;
 	copy->user = pstrdup(orig->user);
 	copy->password = pstrdup(orig->password);
 	copy->nls_lang = pstrdup(orig->nls_lang);

oracle_fdw.h
@@ -61,6 +61,7 @@ struct oracleSession
 	struct srvEntry *srvp;
 	struct connEntry *connp;
 	OCIStmt *stmthp;
+	int have_nchar;
 };
 #endif
 typedef struct oracleSession oracleSession;
@@ -195,7 +196,7 @@ typedef enum
 /*
  * functions defined in oracle_utils.c
  */
-extern oracleSession *oracleGetSession(const char *connectstring, oraIsoLevel isolation_level, char *user, char *password, const char *nls_lang, const char *tablename, int curlevel);
+extern oracleSession *oracleGetSession(const char *connectstring, oraIsoLevel isolation_level, char *user, char *password, const char *nls_lang, int have_nchar, const char *tablename, int curlevel);
 extern void oracleCloseStatement(oracleSession *session);
 extern void oracleCloseConnections(void);
 extern void oracleShutdown(void);

sql/oracle_fdw.sql
@@ -8,7 +8,7 @@ SET client_min_messages = WARNING;
 CREATE EXTENSION oracle_fdw;
 
 -- TWO_TASK or ORACLE_HOME and ORACLE_SID must be set in the server's environment for this to work
-CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '', isolation_level 'read_committed');
+CREATE SERVER oracle FOREIGN DATA WRAPPER oracle_fdw OPTIONS (dbserver '', isolation_level 'read_committed', nchar 'true');
 
 CREATE USER MAPPING FOR PUBLIC SERVER oracle OPTIONS (user 'SCOTT', password 'tiger');
 

commit d67c6b400270f32c06bda03701f85d7b28e3427b

Fix "nchar" bug introduced in 5fd556f602
That commit tried to reduce the impact of the "nchar" option
by only choosing the correct, expensive conversion for
NVARCHAR2 and NCHAR columns.  Unfortunately, an omission in
oracleDescribe() made oracle_fdw think that such columns
were in fact VARCHAR2 and CHAR, so the correct conversion
was never used.

This fixes #706 and (now for real) #416.

Report and analysis by Andrey Teplitskiy.

expected/oracle_fdw.out
@@ -781,7 +781,7 @@ CREATE FOREIGN TABLE badtypes (
 ) SERVER oracle OPTIONS (table 'TYPETEST1');
 -- should fail for column "nc", as "c" is not used
 SELECT id, nc FROM badtypes WHERE id = 1;
-ERROR:  column "nc" (142) of foreign table "badtypes" cannot be converted to or from Oracle data type (1)
+ERROR:  column "nc" (142) of foreign table "badtypes" cannot be converted to or from Oracle data type (3)
 -- this will fail for inserting a NULL in column "c"
 INSERT INTO badtypes (id, nc) VALUES (42, XML '<empty/>');
 ERROR:  column "c" (142) of foreign table "badtypes" cannot be converted to or from Oracle data type (1)

oracle_utils.c
@@ -1110,13 +1110,15 @@ struct oraTable
 		{
 			case SQLT_AFC:
 				/* CHAR(n) */
-				reply->cols[i-1]->oratype = ORA_TYPE_CHAR;
+				reply->cols[i-1]->oratype =
+					(csfrm == SQLCS_NCHAR) ? ORA_TYPE_NCHAR : ORA_TYPE_CHAR;
 				reply->cols[i-1]->val_size = charsize * 4 + 1;
 				break;
 			case SQLT_CHR:
 			case SQLT_VCS:
 				/* VARCHAR(n) and VARCHAR2(n) */
-				reply->cols[i-1]->oratype = ORA_TYPE_VARCHAR2;
+				reply->cols[i-1]->oratype =
+					(csfrm == SQLCS_NCHAR) ? ORA_TYPE_NVARCHAR2 : ORA_TYPE_VARCHAR2;
 				reply->cols[i-1]->val_size = charsize * 4 + 1;
 				break;
 			case SQLT_BLOB: